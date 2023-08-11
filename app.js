4eb7cb186c142fa6240eed501409ccd7540bd832
Krunalmayani <krunalmayani4@gmail.com>
2023-08-11 11:32:33 +0530

    first commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require("cors");
 
@@ -26,7 +27,8 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
+const PORT = process.env.PORT || 5500;
 
-app.listen(5500, function () {
-    console.log('Example app listening on port 5500!');
+app.listen(PORT, () => {
+    console.log(`server is listening  on ${PORT} \n`);
 });
--- db.js
+++ db.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql2');
-
+require('dotenv').config();
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  port: 3306,
-  database: 'ceyron'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  database: process.env.MYSQL_DB
 });
 
 connection.connect(function (err) {
