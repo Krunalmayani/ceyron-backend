dcab58bedaa3ff4b285402b04f58c122153d07ce
krunal mayani <62372141+Krunalmayani@users.noreply.github.com>
2023-08-12 12:21:24 +0530

    Update usersRoutes.js

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,8 +5,7 @@ var multer = require('multer');
 var router = express.Router();
 const bodyParser = require('body-parser');
 const { body } = require('express-validator');
-const { getAllUsers, updateUsers, deleteUsers } = require('../controllers/usersController');
-
+const { getAllUsers, updateUsers, deleteUsers, usersRegister, usersLogin } = require('../controllers/usersController');
 
 
 var forms = multer();
@@ -16,18 +15,37 @@ router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 router.use(forms.array());
 
-
 router.get("/all-users", getAllUsers)
 
-router.put("/:id", [
+router.post('/login', [
+    body('email', "Invalid email address").notEmpty().escape().trim().isEmail(),
+    body('password', "The Password must be of minimum 6 characters length").notEmpty().trim().isLength({ min: 6 }),
+], usersLogin);
 
+router.post("/register", [
+    body('name', 'Name is required').trim().notEmpty().isString().withMessage('Name must be a string')
+        .isLength({ min: 3 }).withMessage('Name must be at least 3 characters')
+        .matches(/^[a-zA-Z\s]+$/).withMessage('Name can only contain letters and spaces'),
+
+    body('email', "Invalid email address").notEmpty().escape().trim().isEmail(),
+
+    body('phone_number', "Mobile Number are Required").notEmpty().escape().trim().isLength({ min: 10, max: 10 }),
+
+    body('password', "The Password must be of minimum 6 characters length").notEmpty().trim().isLength({ min: 6 }),
+
+    body('confirm_password', "The Password must be of minimum 6 characters length").notEmpty().trim().isLength({ min: 6 }),
+
+    body('country').notEmpty().withMessage('Country is required').isLength({ max: 50 }).withMessage('Country cannot exceed 50 characters'),
+], usersRegister);
+
+router.put("/:id", [
     body('user_id', "Users ID is Required").notEmpty().escape().trim(),
     body('name', 'Name is required').trim().notEmpty().isString().withMessage('Name must be a string')
         .isLength({ min: 3 }).withMessage('Name must be at least 3 characters')
         .matches(/^[a-zA-Z\s]+$/).withMessage('Name can only contain letters and spaces'),
     body('email', "Invalid email address").notEmpty().escape().trim().isEmail(),
-    body('phone_no', "Mobile Number are Required").notEmpty().escape().trim().isLength({ min: 10, max: 10 }),
-    body('created_at', 'Date is required').notEmpty().isISO8601().withMessage('Invalid created_at format'),
+    body('phone_number', "Mobile Number are Required").notEmpty().escape().trim().isLength({ min: 10, max: 10 }),
+    body('country').notEmpty().withMessage('Country is required').isLength({ max: 50 }).withMessage('Country cannot exceed 50 characters'),
 ], updateUsers);
 
 router.delete("/:user_id", deleteUsers)
