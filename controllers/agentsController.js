01e715fc448f5626d6df9e25cce020782feab2c5
krunal mayani <62372141+Krunalmayani@users.noreply.github.com>
2023-08-12 10:57:20 +0530

    Update agentsController.js

--- controllers/agentsController.js
+++ controllers/agentsController.js
@@ -2,9 +2,8 @@
 const { validationResult } = require("express-validator");
 const connection = require("../db").promise();
 const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const moment = require('moment');
 const generateUniqueId = require('generate-unique-id');
+const generateToken = require("../untils/generateToken");
 
 
 exports.getAllAgents = async (req, res) => {
@@ -33,105 +32,6 @@ exports.getAllAgents = async (req, res) => {
     }
 }
 
-
-exports.addNewAgent = async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors });
-    }
-    const { name, email, phone_no, branch_name } = req.body
-
-    const token = req?.headers?.authorization?.split(" ")[1];
-    try {
-        if (!token) {
-            return res.json({ success: false, message: "auth Token not found" });
-        }
-        const [row] = await connection.execute("SELECT * FROM agents WHERE `email`=? AND `phone_no`=? ", [email, phone_no]);
-        if (row.length === 0) {
-
-            const agents_id = generateUniqueId({ length: 10, useLetters: false });
-
-            const [rows] = await connection.execute(
-                "INSERT INTO agents(`agents_id`,`name`,`email`,`phone_no`,`branch_name`) VALUES(?,?,?,?,?)",
-                [agents_id, name, email, phone_no, branch_name]
-            );
-
-            if (rows.affectedRows === 1) {
-                const [col] = await connection.execute("SELECT * FROM agents WHERE id=?", [rows.insertId]);
-                return res.json({
-                    success: true,
-                    status: "success",
-                    message: "The user has been successfully inserted...",
-                    data: col[0],
-                });
-            }
-
-        } else {
-            return res.json({ success: false, message: "The E-mail and Mobile Number already in use", });
-        }
-        return res.json({ success: false, message: "Unauthorized accress !", data: row });
-    } catch (error) {
-
-        return res.json({ success: false, message: error.message });
-
-    }
-
-
-}
-
-exports.updateAgents = async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors });
-    }
-    const { id, agents_id, name, email, phone_no, branch_name } = req.body
-
-    const token = req?.headers?.authorization?.split(" ")[1];
-    try {
-        if (!token) {
-            return res.json({ success: false, message: "auth Token not found" });
-        }
-        const [rows] = await connection.execute("UPDATE agents SET agents_id=?,name=?,email=?,phone_no=?,branch_name=? WHERE id=?", [agents_id, name, email, phone_no, branch_name, id])
-        if (rows.affectedRows === 1) {
-            const [row] = await connection.execute('select * from agents WHERE id=?', [id])
-
-            return res.json({ success: true, status: "success", message: 'Agents successfully Update !', data: row[0] })
-        } else {
-            return res.json({ success: false, message: 'Not update !' })
-        }
-    } catch (error) {
-        return res.json({ success: false, error })
-    }
-
-}
-
-
-exports.deleteAgents = async (req, res) => {
-
-    const { id } = req.params;
-
-    const token = req?.headers?.authorization?.split(" ")[1];
-    try {
-        if (!token) {
-            return res.json({ success: false, message: "auth Token not found" });
-        }
-        const [rows] = await connection.execute("DELETE FROM agents WHERE id=?", [id])
-
-
-        if (rows.affectedRows === 1) {
-
-            return res.json({ success: true, status: "success", message: 'Users successfully Delete !', data: id })
-        } else {
-            return res.json({ success: false, message: 'Not update !' })
-        }
-    } catch (error) {
-        console.log('error:', error);
-        return res.json({ success: false, error })
-    }
-}
-
 exports.agentsLogin = async (req, res) => {
     const errors = validationResult(req);
 
@@ -139,9 +39,9 @@ exports.agentsLogin = async (req, res) => {
         return res.status(422).json({ errors: errors.array() });
     }
 
-    const { agents_id, phone_no, password } = req.body;
+    const { agents_id, phone_number, password } = req.body;
     try {
-        const [row] = await connection.execute("SELECT * FROM agents WHERE `agents_id`=? AND phone_no=?", [agents_id, phone_no]);
+        const [row] = await connection.execute("SELECT * FROM agents WHERE `agents_id`=? AND phone_number=?", [agents_id, phone_number]);
 
         if (row.length === 0) {
             return res.json({ success: false, message: "Invalid Mobile Number or agents ID", });
@@ -150,7 +50,7 @@ exports.agentsLogin = async (req, res) => {
             if (!passMatch) {
                 return res.json({ success: false, message: "Incorrect password", });
             } else {
-                const theToken = jwt.sign({ phone_no: phone_no }, "the-super-strong-secrect");
+                const theToken = generateToken(agents_id)
                 if (theToken) {
 
                     const [rows] = await connection.execute("UPDATE agents SET access_token =?  WHERE agents_id=?", [theToken, agents_id]);
@@ -168,6 +68,108 @@ exports.agentsLogin = async (req, res) => {
         return res.json({ success: false, message: error.message });
     }
 
+}
+
+exports.agentsRegister = async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors });
+    }
+
+    const { name, business_name, branch_name, email, phone_number, password, confirm_password, country } = req.body
+    const hash_pass = await bcrypt.hash(password, 12);
+    const theToken = generateToken(email)
+    try {
+        if (theToken) {
+            if (password === confirm_password) {
+                const [row] = await connection.execute("SELECT * FROM agents WHERE `email`=? AND `phone_number`=? ", [email, phone_number]);
+                if (row.length === 0) {
+                    const agents_id = generateUniqueId({ length: 10, useLetters: false });
+                    const [rows] = await connection.execute(
+                        "INSERT INTO agents(`agents_id`,`name`,`business_name`,`branch_name`,`email`,`phone_number`,`password`,`country`,`access_token`) VALUES(?,?,?,?,?,?,?,?,?)",
+                        [agents_id, name, business_name, branch_name, email, phone_number, hash_pass, country, theToken]
+                    );
+                    if (rows.affectedRows === 1) {
+                        const [col] = await connection.execute("SELECT * FROM agents WHERE id=?", [rows.insertId]);
+                        return res.json({
+                            success: true,
+                            status: "success",
+                            message: "Successfully Register...",
+                            data: col[0],
+                            token: theToken,
+                        });
+                    } else {
+                        return res.json({
+                            success: false,
+                            message: "Feild Register !!!.",
+                        });
+                    }
+                } else {
+                    return res.json({ success: false, message: "The E-mail and Mobile Number already in use", });
+                }
+            } else {
+                return res.json({ success: false, message: "Both Password is not Match" });
+            }
+        } else {
+            return res.json({ success: false, message: "jwtToken not generate Please try again !", });
+        }
+
+    } catch (error) {
+        return res.json({ success: false, message: error.message });
+    }
+
+}
+
+exports.updateAgents = async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors });
+    }
+    const { id, agents_id, name, business_name, branch_name, email, phone_number, country } = req.body
+
+    const token = req?.headers?.authorization?.split(" ")[1];
+    try {
+        if (!token) {
+            return res.json({ success: false, message: "auth Token not found" });
+        }
+        const [rows] = await connection.execute("UPDATE agents SET agents_id=?, name=?, business_name=?, branch_name=?, email=?, phone_number=?,country=? WHERE id=?", [agents_id, name, business_name, branch_name, email, phone_number, country, id])
+        if (rows.affectedRows === 1) {
+            const [row] = await connection.execute('select * from agents WHERE id=?', [id])
+
+            return res.json({ success: true, status: "success", message: 'Agents successfully Update !', data: row[0] })
+        } else {
+            return res.json({ success: false, message: 'Not update !' })
+        }
+    } catch (error) {
+        return res.json({ success: false, error })
+    }
+
+}
+
+exports.deleteAgents = async (req, res) => {
+
+    const { id } = req.params;
+
+    const token = req?.headers?.authorization?.split(" ")[1];
+    try {
+        if (!token) {
+            return res.json({ success: false, message: "auth Token not found" });
+        }
+        const [rows] = await connection.execute("DELETE FROM agents WHERE id=?", [id])
+
+
+        if (rows.affectedRows === 1) {
+
+            return res.json({ success: true, status: "success", message: 'Agents successfully Delete !', data: id })
+        } else {
+            return res.json({ success: false, message: 'Not update !' })
+        }
+    } catch (error) {
+        console.log('error:', error);
+        return res.json({ success: false, error })
+    }
 }
 
 exports.forgotPassword = async (req, res) => {
