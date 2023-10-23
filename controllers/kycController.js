d6e7b642115680fb820bebc3adde0db139d9ad52
krunal mayani <62372141+Krunalmayani@users.noreply.github.com>
2023-10-23 11:08:13 +0530

    add secure url

--- controllers/kycController.js
+++ controllers/kycController.js
@@ -15,7 +15,7 @@ exports.kycVerifyData = async (req, res) => {
         return res.json({ success: false, message: 'Image is Not upload!' })
     }
 
-    const url = req.protocol + '://' + process.env.IP_PORT + '/uploads/'
+    const url = process.env.IP_URL + '/uploads/'
 
     const selfie_with_document = url + 'selfies_with_documents/' + req.files.Selfie_with_document[0].filename;
     const front_document = url + 'kyc_front_images/' + req.files.KYC_Front_Image[0].filename;
